907f1e235c259fbe9b52ab31835ea5ebbdcbfb05
Vitória Calvi <calvi.vitoria@gmail.com>
2024-11-30 17:48:39 -0300

    adjusting github actions

--- test/controllers/story_controller_test.rb
+++ test/controllers/story_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class StoryControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    ENV["PUBLIC_KEY"] = "mock_public_key"
+    ENV["PRIVATE_KEY"] = "mock_private_key"
+  end
+
   test "should get index" do
     get story_index_url
     assert_response :success
