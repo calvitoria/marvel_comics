ec64c0620dcce6abffb165a1b1c016ea228bc388
Vitória Calvi <calvi.vitoria@gmail.com>
2024-11-30 11:39:28 -0300

    refactoring interactor

--- app/interactors/story.rb
+++ app/interactors/story.rb
@@ -1,18 +1,27 @@
 class Story
   include Interactor
-  
+
   def call
     marvel_service = MarvelApiService.new
     character_id = context.character_id
-    total_number_of_stories = marvel_service.character_stories(character_id)['total']
-    story = random_story(character_id, total_number_of_stories, marvel_service)
+
+    total_stories = total_stories(character_id, marvel_service)
+
+    if total_stories.positive?
+      context.story = random_story(character_id, total_stories, marvel_service)
+    else
+      context.fail!(error: 'No stories found for the character')
+    end
   end
 
   private
 
-  def random_story(character_id, stories_count, marvel_service)
-    random_story_index = rand(0..stories_count)
+  def total_stories(character_id, marvel_service)
+    marvel_service.character_stories(character_id)['total']
+  end
 
-    marvel_service.character_stories(character_id, offset: random_story_index)
-  end  
+  def random_story(character_id, stories_count, marvel_service)
+    random_index = rand(0..stories_count)
+    marvel_service.character_stories(character_id, offset: random_index)['results']&.first
+  end
 end
