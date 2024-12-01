906f63fd5e4290e420cc59cc9b63a08081303ba3
Vitória Calvi <calvi.vitoria@gmail.com>
2024-12-01 11:46:16 -0300

    Reverting last commit, refactoring
    This reverts commit 207146274ab87e0e530c8923b3ffcf086ce39342.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,9 +2,11 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './app/**/*.html.erb',
+    './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/components/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
