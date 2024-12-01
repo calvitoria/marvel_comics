ec98b2e6282ee71d1058fc591e50d2dc2ae4443f
Vitória Calvi <calvi.vitoria@gmail.com>
2024-12-01 12:32:57 -0300

    add component test

--- test/components/character_component_test.rb
+++ test/components/character_component_test.rb
@@ -3,10 +3,15 @@
 require "test_helper"
 
 class CharacterComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(CharacterComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_component_renders_character_image_and_name
+    image_url = "https://example.com/character.jpg"
+    character_name = "The Hulk"
+
+    rendered = render_inline(CharacterComponent.new(image: image_url, name: character_name))
+
+    assert_includes rendered.css("img").attr("src").value, image_url
+    assert_equal character_name, rendered.css("h3").text.strip
+
+    assert_equal character_name, rendered.css("img").attr("alt").value
   end
 end
