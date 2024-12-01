0e1ac5fca119bb851e440447bf6f1254665ba7c0
Vitória Calvi <calvi.vitoria@gmail.com>
2024-12-01 17:11:32 -0300

    add makefile

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_30_171134) do
+  create_table "stories", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.json "characters"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
